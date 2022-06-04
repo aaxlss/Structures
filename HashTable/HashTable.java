da29f91d513031725b87bcd9caca49db85766bc0
aaxlss <axl9305@gmail.com>
2022-06-04 14:56:11 +0100

    Adding hast table description

---
+++ HashTable/HashTable.java
@@ -0,0 +1,8 @@
+/**
+ * HashTable
+ * maps keys to values pair in hash table
+ * It's synchronized
+ * Java HashTable implements (Interfaces) Map, Serializable, Cloneable  and extendes Dictionary
+ * 
+ * 
+ */
