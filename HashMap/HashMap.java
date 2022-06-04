4a5d79ca7b89addb9f658b5c7979daca4f6067f2
aaxlss <axl9305@gmail.com>
2022-06-04 14:55:54 +0100

    Adding hash map description

---
+++ HashMap/HashMap.java
@@ -0,0 +1,10 @@
+package HashMap;
+/**
+ * HashMap
+ * provide the basi implementation of the Map interface
+ * Stores the data in key value pair.
+ * One object is used as a key to (index) to another object (value)
+ * trying to insert tuplicate key, it will replace the element of the corresponding key.
+ * 
+ * It is unsynchronized. 
+*/
